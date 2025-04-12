5b8b8e90b4df577fec109c11d2a56c55215ae4f5
elle <l88729260@gmail.com>
2025-04-12 18:42:58 +0100

    Create locations.py

---
+++ locations.py
@@ -0,0 +1,16 @@
+locations = [
+"Kensington Gardens",
+"Grosvenor Street",
+"Cytherea Gallery",
+"Royal Opera House",
+"King’s Theatre",
+"Gentleman Jack’s",
+"Gunter’s",
+"Mayfair Ice Cream Parlour & Café",
+"Queen Anne’s Lace & Silk",
+"Lady May’s",
+"The Silver Lantern",
+"The Amorous Bloom",
+"Mayfair Boxing Club",
+"Mayfair Cutlers & Smiths",
+"Pâtissierie à Mayfair"]
